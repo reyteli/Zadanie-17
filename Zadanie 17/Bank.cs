fd73e879812b740601efda7e93b3098015ca5f07
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-23 18:40:39 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 17/Bank.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_17
+{
+    class Bank<T>
+    {
+        private T Schet { get; set; }
+        private string Fio { get; set; }
+        private int Balans { get; set; }
+
+        public Bank(T schet, string fio, int balans)
+        {
+            Schet = schet;
+            Fio = fio;
+            Balans = balans;
+        }
+       
+       public string GetInfo()
+        {
+        return $"{Schet} {Fio} {Balans}";
+        }
+
+    }
+}
---
+++ Zadanie 17/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Zadanie_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер");
+            int schetInt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите фио");
+            string fio = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            int balans = Convert.ToInt32(Console.ReadLine());
+            Bank<int> bank1 = new Bank<int>(schetInt, fio, balans);
+            Console.WriteLine(bank1.GetInfo());
+            Console.WriteLine("Введите строковый номер");
+            string schetString = Console.ReadLine();
+            Console.WriteLine("Введите фио");
+            fio = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            balans = Convert.ToInt32(Console.ReadLine());
+            Bank<string> bank2 = new Bank<string>(schetString, fio, balans);
+            Console.WriteLine(bank2.GetInfo());
+            Console.ReadKey();
+        }
+    }
+}
